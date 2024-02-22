ccbb68c00d374bf127d3b2f40e7320989dea6968
LiKoubra <orqualooping@protonmail.com>
2024-02-22 23:26:09 +0100

    improved readibility

--- src/events/ready.js
+++ src/events/ready.js
@@ -16,27 +16,27 @@ module.exports = {
                 color = (await Vibrant.from(srv.iconURL().slice(0, srv.iconURL().length - '.webp'.length)).getPalette()).Vibrant.hex;
             }
             client.channels.cache.get(servers_channel_id).send({ embeds: [new EmbedBuilder()
-                    .setColor(color)
-                    .setAuthor({
-                        iconURL: srv.iconURL(),
-                        name: srv.name
-                    })
-                    .addFields([
-                        {
-                            name: 'Members',
-                            value: `${srv.members.cache.filter(mmbr => mmbr.user.bot).size}`,
-                            inline: true
-                        }, {
-                            name: 'Interchat',
-                            value: `${srv.channels.cache.filter(chnl => chnl.name.toLowerCase().includes('interchat')).some(() => true)}`,
-                            inline: true
-                        }, {
-                            name: 'Owner',
-                            value: `${client.users.cache.get(srv.ownerId).tag}`,
-                            inline: true
-                        }
-                    ])
-                ] })
+                .setColor(color)
+                .setAuthor({
+                    iconURL: srv.iconURL(),
+                    name: srv.name
+                })
+                .addFields([
+                    {
+                        name: 'Members',
+                        value: `${srv.members.cache.filter(mmbr => mmbr.user.bot).size}`,
+                        inline: true
+                    }, {
+                       name: 'Interchat',
+                       value: `${srv.channels.cache.filter(chnl => chnl.name.toLowerCase().includes('interchat')).some(() => true)}`,
+                       inline: true
+                    }, {
+                       name: 'Owner',
+                       value: `${client.users.cache.get(srv.ownerId).tag}`,
+                       inline: true
+                    }
+                ])
+            ]})
         }
 
     }
